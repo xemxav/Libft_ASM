c8c48b3d22a829677e7cec6665a5a1845dfe353e
Xavier-Emmanuel Moreau <xmoreau@student.le-101.fr>
2020-03-05 13:13:04 +0100

    strcat et strdup ok

--- ft_strcat.s
+++ ft_strcat.s
@@ -11,19 +11,21 @@ _ft_strcat:
 	mov rbp, rsp
 
 	push rdi
-	call _ft_strlen
-	add rdi, rax
-.loop:
+.inc_dest:
+	cmp byte [rdi], 0
+	jz _ft_strcat.writing
+	inc rdi
+	jmp _ft_strcat.inc_dest
+.writing:
 	cmp byte [rsi], 0
-	je _ft_strcat.exit
-	mov rdx, 0
-	mov byte rdx, [rsi]
-	mov [rdi], rdx
+	jz _ft_strcat.end
+	mov rdx,  [rsi]
+	mov  [rdi], rdx
 	inc rdi
 	inc rsi
-	jmp _ft_strcat.loop
+	jmp _ft_strcat.writing
 
-.exit:
+.end:
 	mov byte [rdi], 0
 	pop rax
 	leave
